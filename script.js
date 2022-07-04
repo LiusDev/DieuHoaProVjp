1217016a083b8a3a9175480fa37e75061f0e6b90
QuyDX <wolflavamc@gmail.com>
2022-07-04 20:50:40 +0700

    abc

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-var air = document.getElementById("air"); // Tạo biến air là thẻ div có id="air"
-function turnOn() { // Viết hàm bật điều hoà
-    //Gợi ý: thay đổi thông số opacity của biến air thành 1
-    //Link tham khảo: https://www.w3schools.com/js/js_htmldom_css.asp
+var air = document.getElementById("air");
+function turnOn() { 
     air.style.opacity = 1;
 }
 
-function turnOff() { //Viết hàm tắt điều hoà (tương tự hàm bật)
+function turnOff() {
     air.style.opacity = 0;
 }
